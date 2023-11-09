f85eef116824f1314d24afc842ab20acf69483be
Prakash Chandra Maurya <chandramauryaprakash@gmail.com>
2023-11-09 08:52:13 +0530

    Login Component created & used

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { useState } from 'react';
+// import { useState } from 'react';
 import Data from './Content';
+import Log from './Login';
+import LoginSignup from './Components/LoginSignup/LoginSignup';
+
 // import Card from './Card'
 
 function App() {
-
-
   return (
-    <div className='cont'>
-      {Data.map((ele) => <li ><h3>{ele.title}</h3><p>{ele.body}</p></li>)}
-    </div>
 
+    <div style={{padding:"50px"}}>
+      {/* <Log /> */}
+      <LoginSignup/>
+    </div>
+    // <div>
+    //   <div className='cont'>
+    //     {Data.map((ele) => <li ><h3>{ele.title}</h3><p>{ele.body}</p></li>)}
+    //   </div>
+    //   <div>
+    //     <log />
+    //   </div>
+    // </div>
   );
 }
 
