fa6a62000f25df6d250e284f86ff25505ddf6250
Prakash Chandra Maurya <chandramauryaprakash@gmail.com>
2023-12-02 10:53:07 +0530

    connectaAPI

--- src/App.js
+++ src/App.js
@@ -1,29 +1,56 @@
-// import logo from './logo.svg';
-import React from 'react';
-import './App.css';
-// import { useState } from 'react';
-import Data from './Content';
-import Log from './Login';
-import LoginSignup from './Components/LoginSignup/LoginSignup';
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./compenets/header/Home";
+import Contact from "./compenets/header/Contact";
+import About from "./compenets/header/About";
+import { Link } from "react-router-dom";
+import "./App.css";
+import Drop from "./compenets/header";
+import Generic from "./compenets/Geners";
 
-// import Card from './Card'
+const App = () => {
+  const [fetchDrop, setDrop] = useState([]);
+  const FetchList = () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MDJlM2NlMTMyNzE0ODA5ZmNmYjAxMTM3MGY1MDQ2MCIsInN1YiI6IjY1NTZmNmRkZDRmZTA0MDExYjkyMWEzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.W7CRFpf1rRRELyq2LzxbNbBnDlR5lv2KejSV_pVBYc4",
+      },
+    };
 
-function App() {
+    fetch("https://api.themoviedb.org/3/genre/movie/list?language=en", options)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setDrop(data.genres);
+      })
+      .catch((err) => console.error(err));
+  };
+  useEffect(() => {
+    FetchList();
+  }, []);
+
   return (
-
-    <div style={{padding:"50px"}}>
-      {/* <Log /> */}
-      <LoginSignup/>
-    </div>
-    // <div>
-    //   <div className='cont'>
-    //     {Data.map((ele) => <li ><h3>{ele.title}</h3><p>{ele.body}</p></li>)}
-    //   </div>
-    //   <div>
-    //     <log />
-    //   </div>
-    // </div>
+    <>
+      <header>
+        <h1>Movies</h1>
+        <div className="navmenu">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Drop fetchDrop={fetchDrop} />
+        </div>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Contact />} />
+        <Route path="/contact" element={<About />} />
+        <Route path="/movies/gen/:id" element={<Generic />} />
+      </Routes>
+      <footer>THis is footer</footer>
+    </>
   );
-}
-
+};
 export default App;
